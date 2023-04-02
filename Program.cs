cc47812436fcda64fdc09c1d57e6b8e90a8fe5cb
AndreJeanDias <128902291+AndreJeanDias@users.noreply.github.com>
2023-04-02 11:16:09 -0300

    Cidades
    Exibindo nome das cidades do litoral sul de SP.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Peruíbe");
+Console.WriteLine("Ilha Comprida");
+Console.WriteLine("Registro");
+Console.WriteLine("Iguape");
+Console.WriteLine("Cananéia");
+Console.WriteLine("Sete Barras");
+Console.WriteLine("Eldorado");
